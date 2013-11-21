b28e095e8cb8d495f45779c69e8c71fdb3899b20
David Paschich <dpassage@balveda.com>
2013-11-20 20:02:41 -0800

    First commit: rails4 setup with postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Parkingapi::Application.config.secret_key_base = '7b43cadc3f6bf902c1043457e2b374febc817b53802c1f8173f1adb65a2edd84f2d348cf1412e03c0240e9df725e2acee1762f82f4ef5ec41a1f3e340a5ff2f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Parkingapi::Application.config.session_store :cookie_store, key: '_parkingapi_session'
