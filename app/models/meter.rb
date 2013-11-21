0320931e64e960950064125da3ba555e7c5a7067
David Paschich <dpassage@balveda.com>
2013-11-20 20:23:14 -0800

    First working geospatial test!

--- app/models/meter.rb
+++ app/models/meter.rb
@@ -1,2 +1,13 @@
 class Meter < ActiveRecord::Base
+  scope :close_to, -> (latitude, longitude, distance_in_meters = 2000) {
+    where(%{
+      ST_DWithin(
+        ST_GeographyFromText(
+          'SRID=4326;POINT(' || meters.longitude || ' ' || meters.latitude || ')'
+        ),
+        ST_GeographyFromText('SRID=4326;POINT(%f %f)'),
+      %d
+      )
+    } % [longitude, latitude, distance_in_meters])
+  }
 end
--- test/models/meter_test.rb
+++ test/models/meter_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class MeterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "close meters" do
+    far_cafe = Meter.create!(
+      name:      "Far Meter",
+      latitude:   40.000000,
+      longitude: -77.000000
+    )
+
+    close_meter = Meter.create!(
+      name:      "Close Meter",
+      latitude:   39.010000,
+      longitude: -75.990000
+    )
+
+    close_meters = Meter.close_to(39.000000, -76.000000).load
+
+    assert_equal 1,          close_meters.size
+    assert_equal close_meter, close_meters.first
+  end
 end
